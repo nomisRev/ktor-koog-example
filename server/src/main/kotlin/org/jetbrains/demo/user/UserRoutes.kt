448df414519901d8850db487fac8988748bfe39f
Simon Vergauwen <nomisRev@users.noreply.github.com>
2025-08-26 10:10:08 +0200

    Temp disable Auth until local dev UX is fixed

--- server/src/main/kotlin/org/jetbrains/demo/user/UserRoutes.kt
+++ server/src/main/kotlin/org/jetbrains/demo/user/UserRoutes.kt
@@ -17,43 +17,41 @@ import io.ktor.server.routing.routing
 import io.ktor.server.util.getOrFail
 
 fun Application.userRoutes(users: UserRepository) = routing {
-    authenticate("google") {
-        route("user") {
-            post("/register") {
-                val userInfo =
-                    call.principal<OpenIdConnectPrincipal.UserInfo>() ?: return@post call.respond(HttpStatusCode.Unauthorized)
-                val user = users.create(userInfo.subject)
-                call.respond(HttpStatusCode.OK, user)
-            }
+    route("user") {
+        post("/register") {
+            val userInfo =
+                call.principal<OpenIdConnectPrincipal.UserInfo>() ?: return@post call.respond(HttpStatusCode.Unauthorized)
+            val user = users.create(userInfo.subject)
+            call.respond(HttpStatusCode.OK, user)
+        }
 
-            get("/") {
-                val userInfo =
-                    call.principal<OpenIdConnectPrincipal.UserInfo>() ?: return@get call.respond(HttpStatusCode.Unauthorized)
-                val user = users.findOrNull(userInfo.subject)
-                if (user == null) call.respond(HttpStatusCode.NotFound)
-                else call.respond(HttpStatusCode.OK, user)
-            }
+        get("/") {
+            val userInfo =
+                call.principal<OpenIdConnectPrincipal.UserInfo>() ?: return@get call.respond(HttpStatusCode.Unauthorized)
+            val user = users.findOrNull(userInfo.subject)
+            if (user == null) call.respond(HttpStatusCode.NotFound)
+            else call.respond(HttpStatusCode.OK, user)
+        }
 
-            get("/email/{email}") {
-                val email = call.parameters.getOrFail("email")
-                val user = users.findByEmail(email) ?: return@get call.respond(HttpStatusCode.NotFound)
-                call.respond(HttpStatusCode.OK, user)
-            }
+        get("/email/{email}") {
+            val email = call.parameters.getOrFail("email")
+            val user = users.findByEmail(email) ?: return@get call.respond(HttpStatusCode.NotFound)
+            call.respond(HttpStatusCode.OK, user)
+        }
 
-            put("/update") {
-                val userInfo =
-                    call.principal<OpenIdConnectPrincipal.UserInfo>() ?: return@put call.respond(HttpStatusCode.Unauthorized)
-                val update = call.receive<UpdateUser>()
-                val updatedUser = users.create(userInfo.subject, update)
-                call.respond(HttpStatusCode.OK, updatedUser)
-            }
+        put("/update") {
+            val userInfo =
+                call.principal<OpenIdConnectPrincipal.UserInfo>() ?: return@put call.respond(HttpStatusCode.Unauthorized)
+            val update = call.receive<UpdateUser>()
+            val updatedUser = users.create(userInfo.subject, update)
+            call.respond(HttpStatusCode.OK, updatedUser)
+        }
 
-            delete("/delete") {
-                val userInfo =
-                    call.principal<OpenIdConnectPrincipal.UserInfo>() ?: return@delete call.respond(HttpStatusCode.Unauthorized)
-                users.delete(userInfo.subject)
-                call.respond(HttpStatusCode.OK)
-            }
+        delete("/delete") {
+            val userInfo =
+                call.principal<OpenIdConnectPrincipal.UserInfo>() ?: return@delete call.respond(HttpStatusCode.Unauthorized)
+            users.delete(userInfo.subject)
+            call.respond(HttpStatusCode.OK)
         }
     }
 }
